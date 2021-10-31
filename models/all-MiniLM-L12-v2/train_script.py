49fbebdcca6d3bb020f59038662ba4508accb559
Adlyn Heng <adlynheng@gmail.com>
2021-10-31 17:52:57 +0800

    Add comment to train_script.py.

--- models/all-MiniLM-L12-v2/train_script.py
+++ models/all-MiniLM-L12-v2/train_script.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:176dd4e16a71a0e5cf76cefb0519fbc130e7ac3ee0b7ad707ef24067e6514be5
-size 13159
+oid sha256:52b5c5237baef1eb68cc1f12812ec0881a26921fd6febe9d8519fce4a29e4756
+size 13372
